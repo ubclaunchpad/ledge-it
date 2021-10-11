a634340da952f419c0db64336089ee461ed1eb2c
Kavpreet Grewal <kavpreetgrewal@gmail.com>
2021-10-10 23:35:44 -0700

    Initialize React Native client side

---
+++ client/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb',
+    '@react-native-community',
+    'plugin:import/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', 'prettier'],
+  rules: {
+    indent: ['error', 2, { SwitchCase: 1 }],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'prettier/prettier': 'error',
+    'react/style-prop-object': 0,
+  },
+};
