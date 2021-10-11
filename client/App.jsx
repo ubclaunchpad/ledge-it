5784a3d40621dfc3ac41274509d7c3fa4846d6dd
Kavpreet Grewal <kavpreetgrewal@gmail.com>
2021-10-10 23:36:17 -0700

    Initialize React Native client side

--- client/App.jsx
+++ client/App.jsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.jsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
