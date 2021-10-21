38fec2fa62648222f3a49c9c682868be421fbf93
Ilya Patrzykat-Hannas <54457854+patrzykat@users.noreply.github.com>
2021-10-21 13:15:40 -0700

    Apply suggestions from code review
    Co-authored-by: Kavpreet Grewal <kavpreetgrewal@gmail.com>

--- server/src/models/Expense.py
+++ server/src/models/Expense.py
@@ -3,14 +3,20 @@ from pydantic import BaseModel
 # Location is stored as an x and y coordinate (easier to input into maps APIs)
 # All fields are currently required
 class Expense(BaseModel):
-    id: int
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    # .... the rest of the fields
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
     name: str
-    description: str
+    description: Optional[str] = None
     date: date
     price: float
     currency: str 
     exchange_rate: float 
-    locationx: float
-    locationy: float 
+    location: Optional[str] = None
+    locationx: Optional[float] = None
+    locationy: Optiona[float] = None
     category: str 
-    sub_category: str
+    sub_category: Optional[str] = None
