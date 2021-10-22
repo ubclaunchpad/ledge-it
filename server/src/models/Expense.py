78d47d02156d74f1735906d7b1a6c4f5c10d7644
Ilya Patrzykat-Hannas <54457854+patrzykat@users.noreply.github.com>
2021-10-22 08:40:13 -0700

    Update Expense.py

--- server/src/models/Expense.py
+++ server/src/models/Expense.py
@@ -4,19 +4,19 @@ from pydantic import BaseModel
 # All fields are currently required
 class Expense(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
-    # .... the rest of the fields
-    class Config:
-        allow_population_by_field_name = True
-        arbitrary_types_allowed = True
-        json_encoders = {ObjectId: str}
     name: str
     description: Optional[str] = None
     date: date
     price: float
-    currency: str 
-    exchange_rate: float 
+    currency: str
+    exchange_rate: float
     location: Optional[str] = None
     locationx: Optional[float] = None
     locationy: Optiona[float] = None
-    category: str 
+    category: str
     sub_category: Optional[str] = None
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
