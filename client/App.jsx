9f189ab4ed1b29fc182c005bb52ff25ec302e587
Michelle Kim <yeojin011016@gmail.com>
2021-10-24 17:47:13 -0700

    added basic navbar structure, needs to deal with few errors

--- client/App.jsx
+++ client/App.jsx
@@ -1,15 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomePage from './src/pages/HomePage';
+import TablePage from './src/pages/TablePage';
+import BudgetPage from './src/pages/BudgetPage';
+import AnalyticsPage from './src/pages/AnalyticsPage';
+import SettingsPage from './src/pages/SettingsPage';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.jsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Open up App.jsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+      <TabNavBar />
+    </NavigationContainer>
   );
 };
+
+const Tab = createBottomTabNavigator();
+
+function TabNavBar() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" compotent={HomePage} />
+      <Tab.Screen name="Table" compotent={TablePage} />
+      <Tab.Screen name="Budget" compotent={BudgetPage} />
+      <Tab.Screen name="Analytics" compotent={AnalyticsPage} />
+      <Tab.Screen name="Settings" compotent={SettingsPage} />
+    </Tab.Navigator>
+  );
+}
 
 export default App;
 
