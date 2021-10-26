6fcf939aec1f64079f1a1c12c5090d304f6c6acd
Michelle Kim <yeojin011016@gmail.com>
2021-10-26 01:46:37 -0700

    linter code formatting

--- client/App.jsx
+++ client/App.jsx
@@ -1,6 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomePage from './src/pages/HomePage';
@@ -32,12 +30,3 @@ function TabNavBar() {
 }
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
