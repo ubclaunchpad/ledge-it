ab8bfd7cf0fcff511c067a7f1fadda2e36b34b80
patrzykat <ilyapatrzykat@gmail.com>
2021-11-03 15:26:02 -0700

    Added FAB

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import ActionButton from '../components/ActionButton';
 
 const HomePage = () => {
   return (
-    <View>
-      <Text>Home Page</Text>
-      <Text>This is the home page</Text>
-    </View>
+    <React.Fragment>
+      <View>
+        <Text>Home Page</Text>
+        <Text>This is the home page</Text>
+      </View>
+      <ActionButton />
+    </React.Fragment>
   );
 };
 
