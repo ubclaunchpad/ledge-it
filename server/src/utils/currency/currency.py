cc0902331b4e74fdd72c92183c993df9ab2f73c2
UBC Student <rafa3l389@gmail.com>
2021-11-05 15:36:43 -0700

    Imported stuff from previous branch

---
+++ server/src/utils/currency/currency.py
@@ -0,0 +1,48 @@
+# Dependencies:
+# Need to run python3 -m pip install requests 
+import requests
+import json
+from currencyList import is_Currency
+
+# EFFECT: Returns the exchange rate when given 2 currencies
+
+API_KEY = "b248938740450b2e6e0616db"
+
+# Request input
+
+while True:
+    try:
+        curr_from = input("Enter foreign currency: ") # Eg: USD
+        if (is_Currency(curr_from) == False):
+            raise Exception
+        break
+    except Exception:
+        print("Not a valid currency. Please try again...")
+
+while True:
+    try:
+        amount = int(input("Enter the amount to convert: "))
+        break
+    except ValueError:
+        print("Not a valid number. Please try again...")
+
+while True:
+    try:
+        curr_to = input("Enter base currency: ") # Eg: CAD
+        if (is_Currency(curr_to) == False):
+            raise Exception
+        break
+    except Exception:
+        print("Not a valid currency. Please try again...")
+
+# Concatenate URL string
+URL = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/pair/"
+URL = URL + curr_from + "/" + curr_to + "/" + str(amount)
+
+# Send GET request to URL link
+response = requests.get(URL)
+output = response.json()
+
+# Parsing the json
+print("Today's Exchange rate from " + curr_from + " to " + curr_to + ": " + str(output["conversion_rate"]))
+print("Amount converted to: " + str(output["conversion_result"])+ " " + curr_to)
---
+++ server/src/utils/currency/currencyList.py
@@ -0,0 +1,49 @@
+# Dependencies:
+# Need to run python3 -m pip install requests 
+import requests
+import json
+
+# Setup URL string
+
+# EFFECT: Returns the list of currencies in the form of an array
+def currency_list():
+    API_KEY = "b248938740450b2e6e0616db"
+
+    URL = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/latest/CAD"
+
+    # Send GET request to URL link
+    response = requests.get(URL)
+    output = response.json()
+
+    # Parsing the json
+    currencies = output["conversion_rates"]
+    currNames = []
+    for currName, value in currencies.items():
+        currNames.append(currName)
+
+    return currNames
+
+# EFFECT: Returns whether the string is a currency or not
+
+def is_Currency(inputString):
+    API_KEY = "b248938740450b2e6e0616db"
+
+    URL = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/latest/CAD"
+
+    # Send GET request to URL link
+    response = requests.get(URL)
+    output = response.json()
+
+    # Parsing the json
+    currencies = output["conversion_rates"]
+    currencyNames = []
+    for currName, value in currencies.items():
+        currencyNames.append(currName)
+
+    if inputString in currencyNames:
+        return True
+    else: 
+        return False
+
+    
+
