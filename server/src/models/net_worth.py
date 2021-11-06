1b1eb9fd449823956c4c5d63cf67e4c0528b8e5b
James Magee <james@dhcp-128-189-229-225.ubcsecure.wireless.ubc.ca>
2021-11-05 19:57:39 -0700

    ran black .

--- server/src/models/net_worth.py
+++ server/src/models/net_worth.py
@@ -2,6 +2,7 @@ from typing import Optional
 from datetime import date
 from .mongo_db_model import MongoDBModel
 
+
 class NetWorth(MongoDBModel):
     current: float
     history: dict[date, float]
