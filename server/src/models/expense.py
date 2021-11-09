78c799aeaebef8d75b04a2f7422795f82678d53c
kumarpit <kumar.arpit77@yahoo.com>
2021-11-08 23:11:27 -0800

    calls exchange rate api for incomes/expenses

--- server/src/models/expense.py
+++ server/src/models/expense.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from datetime import date
 from .mongo_db_model import MongoDBModel
+from pydantic import validator
 
 
 class Expense(MongoDBModel):
@@ -16,6 +17,12 @@ class Expense(MongoDBModel):
     category: str
     sub_category: Optional[str] = None
 
+    @validator("exchange_rate")
+    def must_not_be_equal_negative_one(cls, exr):
+        if exr == -1:
+            raise ValueError("invalid currency")
+        return exr
+
 
 class UpdateExpenseModel(MongoDBModel):
     name: Optional[str]
@@ -29,3 +36,26 @@ class UpdateExpenseModel(MongoDBModel):
     location_y: Optional[float]
     category: Optional[str]
     sub_category: Optional[str]
+
+    @validator("exchange_rate")
+    def must_not_be_equal_negative_one(cls, exr):
+        if exr is not None and exr == -1:
+            raise ValueError("invalid currency")
+        return exr
+
+    class Config:
+        validate_assignment = True
+
+
+class AddExpense(MongoDBModel):
+    name: str
+    description: Optional[str] = None
+    date: date
+    price: float
+    currency: str
+    exchange_rate: Optional[float]
+    location: Optional[str] = None
+    location_x: Optional[float] = None
+    location_y: Optional[float] = None
+    category: str
+    sub_category: Optional[str] = None
