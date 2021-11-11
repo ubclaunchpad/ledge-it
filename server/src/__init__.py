0ccc1668959bfab567aa0e982384e8fce7c53652
James Magee <james@dhcp-128-189-227-138.ubcsecure.wireless.ubc.ca>
2021-11-11 13:48:49 -0800

    ran black .

--- server/src/__init__.py
+++ server/src/__init__.py
@@ -2,5 +2,3 @@ from .models import *
 from .routes import *
 from .utils import *
 from .database import *
-
-
--- server/src/models/net_worth.py
+++ server/src/models/net_worth.py
@@ -3,12 +3,20 @@ from .mongo_db_model import MongoDBModel
 from typing import List, Tuple
 from datetime import date
 
+
 class NetWorth(MongoDBModel):
     current: float
-    history: list[dict] # History is list of [{date: date, value: networth}] -- NOT change in net worth. 
+    history: list[
+        dict
+    ]  # History is list of [{date: date, value: networth}] -- NOT change in net worth.
 
     class Config:
         schema_extra = {
-            "example": {"current": "100.00", "history": [{"date": "2020-01-01", "value": "120.00"}, {"date": "2020-01-02", "value": "140.00"}]}
+            "example": {
+                "current": "100.00",
+                "history": [
+                    {"date": "2020-01-01", "value": "120.00"},
+                    {"date": "2020-01-02", "value": "140.00"},
+                ],
+            }
         }
-
