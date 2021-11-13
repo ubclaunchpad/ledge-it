9a424c2ff9f3b2b94cb2cf9b398050823269ea11
UBC Student <rafa3l389@gmail.com>
2021-11-13 12:32:12 -0800

    Ran linter

--- client/src/components/TablePage/ScrollTable.jsx
+++ client/src/components/TablePage/ScrollTable.jsx
@@ -2,39 +2,43 @@ import React from 'react';
 import { StyleSheet, ScrollView, SafeAreaView, StatusBar } from 'react-native';
 import TableComponent from './TableComponent';
 
-const ScrollTable = props => (
-    <ScrollTableHelper renderList = {props.renderList} type = {props.type}/>
+const ScrollTable = (props) => (
+  <ScrollTableHelper renderList={props.renderList} type={props.type} />
 );
 
 export default ScrollTable;
 
-ScrollTableHelper = (props) =>{
-    const type = props.type
-    const componentList = props.renderList.map((monthExp, index) => 
-        <TableComponent key = {index} title = {monthExp.month} subTitle = {monthExp.year} mult = {monthExp.list} type = {type}></TableComponent>
-    )
-    
-    return (
-        <SafeAreaView style={styles.container}>
-            <ScrollView style={styles.scrollView}>
-                {componentList}
-            </ScrollView>
-        </SafeAreaView>
-    );
-}
+ScrollTableHelper = (props) => {
+  const { type } = props;
+  const componentList = props.renderList.map((monthExp, index) => (
+    <TableComponent
+      key={index}
+      title={monthExp.month}
+      subTitle={monthExp.year}
+      mult={monthExp.list}
+      type={type}
+    />
+  ));
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>{componentList}</ScrollView>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      paddingTop: StatusBar.currentHeight,
-    },
-    scrollView: {
-      backgroundColor: '#244fad',
-      marginHorizontal: 20,
-      borderRadius: 10
-    },
-    text: {
-      fontSize: 42,
-      margin: 8
-    },
-  });
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+  },
+  scrollView: {
+    backgroundColor: '#244fad',
+    marginHorizontal: 20,
+    borderRadius: 10,
+  },
+  text: {
+    fontSize: 42,
+    margin: 8,
+  },
+});
--- client/src/pages/TablePage.jsx
+++ client/src/pages/TablePage.jsx
@@ -1,139 +1,142 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, StatusBar, Picker } from 'react-native';
+import React, { useState } from 'react';
+import { Picker } from 'react-native';
 import ScrollTable from '../components/TablePage/ScrollTable';
 
 // Note: These are just sample data of expense/income from the database.
 
-const expenseDatabase = [{name: 'Gym', 
-                          price: '65.00', 
-                          date: new Date("2021-10-21T10:34:23"),
-                          currency: 'CAD',
-                          category: 'Category #41'
-                          },
-                          {name: 'Coffee', 
-                          price: '4.29', 
-                          date: new Date("2021-10-17T10:34:23"),
-                          currency: 'CAD',
-                          category: 'Category #12'
-                          },
-                          {name: 'Monitor', 
-                          price: '205.00', 
-                          date: new Date("2021-09-27T10:34:23"),
-                          currency: 'CAD',
-                          category: 'Category #13'
-                          },
-                          {name: 'Mouse', 
-                          price: '54.99', 
-                          date: new Date("2021-09-05T10:34:23"),
-                          currency: 'CAD',
-                          category: 'Category #22'
-                          },
-                          {name: 'Skateboard', 
-                          price: '80.99', 
-                          date: new Date("2021-08-17T10:34:23"),
-                          currency: 'USD',
-                          category: 'Category #25'
-                          }
-                          ]
+const expenseDatabase = [
+  {
+    name: 'Gym',
+    price: '65.00',
+    date: new Date('2021-10-21T10:34:23'),
+    currency: 'CAD',
+    category: 'Category #41',
+  },
+  {
+    name: 'Coffee',
+    price: '4.29',
+    date: new Date('2021-10-17T10:34:23'),
+    currency: 'CAD',
+    category: 'Category #12',
+  },
+  {
+    name: 'Monitor',
+    price: '205.00',
+    date: new Date('2021-09-27T10:34:23'),
+    currency: 'CAD',
+    category: 'Category #13',
+  },
+  {
+    name: 'Mouse',
+    price: '54.99',
+    date: new Date('2021-09-05T10:34:23'),
+    currency: 'CAD',
+    category: 'Category #22',
+  },
+  {
+    name: 'Skateboard',
+    price: '80.99',
+    date: new Date('2021-08-17T10:34:23'),
+    currency: 'USD',
+    category: 'Category #25',
+  },
+];
 
-const incomeDatabase = [{name: 'Part Time: Piano Lessons', 
-                          price: '650.00', 
-                          date: new Date("2021-10-21T10:34:23"),
-                          currency: 'CAD',
-                          category: 'Category #1'
-                          },
-                          {name: 'Tax Refunds', 
-                          price: '205.00', 
-                          date: new Date("2021-09-27T10:34:23"),
-                          currency: 'CAD',
-                          category: 'Category #13'
-                          },
-                          {name: 'Part Time: CPSC 312 TA', 
-                          price: '1344.99', 
-                          date: new Date("2021-09-05T10:34:23"),
-                          currency: 'CAD',
-                          category: 'Category #1'
-                          },
-                          {name: 'Monthly Allowance', 
-                          price: '80.99', 
-                          date: new Date("2021-08-17T10:34:23"),
-                          currency: 'USD',
-                          category: 'Category #25'
-                          }
-                          ]
-
-
-
+const incomeDatabase = [
+  {
+    name: 'Part Time: Piano Lessons',
+    price: '650.00',
+    date: new Date('2021-10-21T10:34:23'),
+    currency: 'CAD',
+    category: 'Category #1',
+  },
+  {
+    name: 'Tax Refunds',
+    price: '205.00',
+    date: new Date('2021-09-27T10:34:23'),
+    currency: 'CAD',
+    category: 'Category #13',
+  },
+  {
+    name: 'Part Time: CPSC 312 TA',
+    price: '1344.99',
+    date: new Date('2021-09-05T10:34:23'),
+    currency: 'CAD',
+    category: 'Category #1',
+  },
+  {
+    name: 'Monthly Allowance',
+    price: '80.99',
+    date: new Date('2021-08-17T10:34:23'),
+    currency: 'USD',
+    category: 'Category #25',
+  },
+];
 
 const TablePage = () => {
-  const[splitExpenses, setSplitExpenses] = useState([]);
-  const[splitIncome, setSplitIncome] = useState([]);
-  const[pickerValue, setPickerValue] = useState('Expenses')
+  const [splitExpenses, setSplitExpenses] = useState([]);
+  const [splitIncome, setSplitIncome] = useState([]);
+  const [pickerValue, setPickerValue] = useState('Expenses');
 
-  expenseDatabase.forEach(expense => {
+  expenseDatabase.forEach((expense) => {
     const month = expense.date.getMonth() + 1;
     const year = expense.date.getFullYear();
-    if(splitExpenses.length == 0){
-      splitExpenses.push(
-        {
-        month: month,
-        year: year,
-        list: []
-        }
-      )
+    if (splitExpenses.length === 0) {
+      splitExpenses.push({
+        month,
+        year,
+        list: [],
+      });
     }
-    if (splitExpenses[splitExpenses.length-1].month === month){
-      splitExpenses[splitExpenses.length-1].list.push(expense)
+    if (splitExpenses[splitExpenses.length - 1].month === month) {
+      splitExpenses[splitExpenses.length - 1].list.push(expense);
     } else {
-      splitExpenses.push(
-        {
-        month: month,
-        year: year,
-        list: [expense]
-        }
-      )
+      splitExpenses.push({
+        month,
+        year,
+        list: [expense],
+      });
     }
-  })
+  });
 
-  incomeDatabase.forEach(income => {
+  incomeDatabase.forEach((income) => {
     const month = income.date.getMonth() + 1;
     const year = income.date.getFullYear();
-    if(splitIncome.length == 0){
-      splitIncome.push(
-        {
-        month: month,
-        year: year,
-        list: []
-        }
-      )
+    if (splitIncome.length === 0) {
+      splitIncome.push({
+        month,
+        year,
+        list: [],
+      });
     }
-    if (splitIncome[splitIncome.length-1].month === month){
-      splitIncome[splitIncome.length-1].list.push(income)
+    if (splitIncome[splitIncome.length - 1].month === month) {
+      splitIncome[splitIncome.length - 1].list.push(income);
     } else {
-      splitIncome.push(
-        {
-        month: month,
-        year: year,
-        list: [income]
-        }
-      )
+      splitIncome.push({
+        month,
+        year,
+        list: [income],
+      });
     }
-  })
-
-
+  });
 
   return (
     <>
       <Picker
-      selectedValue = {pickerValue}
-      onValueChange = {(chosenValue) => {setPickerValue(chosenValue)}}>
-        <Picker.Item label = "Expenses" value = "Expenses"/>
-        <Picker.Item label = "Income" value = "Income"/>
+        selectedValue={pickerValue}
+        onValueChange={(chosenValue) => {
+          setPickerValue(chosenValue);
+          setSplitExpenses([]);
+          setSplitIncome([]);
+        }}>
+        <Picker.Item label="Expenses" value="Expenses" />
+        <Picker.Item label="Income" value="Income" />
       </Picker>
-      {pickerValue =='Expenses'
-          ? <ScrollTable renderList ={splitExpenses} type = {pickerValue}></ScrollTable> 
-          : <ScrollTable renderList ={splitIncome} type = {pickerValue}></ScrollTable> 
-      }
+      {pickerValue === 'Expenses' ? (
+        <ScrollTable renderList={splitExpenses} type={pickerValue} />
+      ) : (
+        <ScrollTable renderList={splitIncome} type={pickerValue} />
+      )}
     </>
   );
 };
