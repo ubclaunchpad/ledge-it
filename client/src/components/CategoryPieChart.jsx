8851467bea80d22ea9983285f35ec046c94a27e3
Michelle Kim <yeojin011016@gmail.com>
2021-11-19 21:29:39 -0800

    added category chart and made it scrollable

--- client/src/components/CategoryPieChart.jsx
+++ client/src/components/CategoryPieChart.jsx
@@ -1,16 +1,56 @@
-import React, { Component } from 'react';
-
+import React from 'react';
+import { StyleSheet, FlatList, Text, SafeAreaView, ScrollView } from 'react-native';
 import { VictoryPie } from 'victory-native';
 
 const CategoryPieChart = () => {
-  return <VictoryPie innerRadius={100} data={sampleData} />;
+  return (
+    <SafeAreaView>
+      <FlatList
+        data={sampleData}
+        renderItem={({ item }) => <Text style={styles.item_style}>{item.x}</Text>}
+        keyExtractor={(item, index) => index.toString()}
+        ListHeaderComponent={getHeader}
+      />
+    </SafeAreaView>
+  );
+};
+
+const getHeader = () => {
+  return (
+    <VictoryPie
+      innerRadius={100}
+      data={sampleData}
+      colorScale={sampleColor}
+      style={{ labels: { fill: '#FFFFFF00' } }}
+    />
+  );
 };
 
 const sampleData = [
-  { x: 'A', y: 40 },
-  { x: 'B', y: 35 },
-  { x: 'C', y: 25 },
-  { x: 'D', y: 30 },
+  { x: 'Category 1', y: 20 },
+  { x: 'Category 2', y: 20 },
+  { x: 'Category 3', y: 20 },
+  { x: 'Category 4', y: 20 },
+  { x: 'Category 5', y: 10 },
+  { x: 'Amount Left', y: 10 },
 ];
+
+const sampleColor = ['#FF5E5E', '#D9BBF1', '#92F889', '#FFC36A', '#6DA8FF', 'grey'];
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  item_style: {
+    backgroundColor: '#B591FF',
+    padding: 20,
+    marginVertical: 3,
+    marginHorizontal: 16,
+    borderRadius: 5,
+  },
+});
 
 export default CategoryPieChart;
