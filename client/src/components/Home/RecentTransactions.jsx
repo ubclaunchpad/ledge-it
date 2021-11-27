96a5fd091fab9e77696fd9ac80a3444031a1b5ac
Michelle Kim <yeojin011016@gmail.com>
2021-11-27 12:10:53 -0800

    adjusted to use the databases from TablePage

--- client/src/components/Home/RecentTransactions.jsx
+++ client/src/components/Home/RecentTransactions.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { StyleSheet, Text, SafeAreaView, View } from 'react-native';
 
 const RecentTransactions = () => {
+  // merge expense and income dataset and come up with an array sorted by date
+  // only show 10 items max
+  const transactionsToDisplay = getTransactionsToDisplay();
+
   return (
     <SafeAreaView style={styles.centeredView}>
       <Text style={styles.title}>Recent transactions</Text>
@@ -11,7 +15,7 @@ const RecentTransactions = () => {
           <Text style={styles.labelText}>Category</Text>
           <Text style={styles.labelText}>Amount</Text>
         </View>
-        {sampleData.map((item, index) => {
+        {transactionsToDisplay.map((item, index) => {
           return (
             <View style={styles.card} key={item.id}>
               <View style={styles.cardLeft}>
@@ -21,7 +25,7 @@ const RecentTransactions = () => {
                 <Text style={styles.cardText}>{item.category}</Text>
               </View>
               <View style={styles.cardRight}>
-                <Text style={styles.cardText}>${item.amount.toFixed(2)}</Text>
+                <Text style={styles.cardText}>{item.price}</Text>
               </View>
             </View>
           );
@@ -31,14 +35,139 @@ const RecentTransactions = () => {
   );
 };
 
-const sampleData = [
-  { id: 1, name: 'Name', category: 'Category 1', amount: 25.0 },
-  { id: 2, name: 'Name', category: 'Category 1', amount: 20.1 },
-  { id: 3, name: 'Name', category: 'Category 2', amount: 250.0 },
-  { id: 4, name: 'Name', category: 'Category 2', amount: 20.0 },
-  { id: 5, name: 'Name', category: 'Category 3', amount: 10.75 },
-  { id: 6, name: 'Name', category: 'Category 1', amount: 20.25 },
+// data taken directly from "../pages/TablePage"
+const expenseDatabase = [
+  {
+    id: 0,
+    name: 'Gym',
+    price: '65.00',
+    date: new Date('2022-10-21T10:34:23'),
+    currency: 'CAD',
+    category: 'Category #41',
+  },
+  {
+    id: 1,
+    name: 'Gym',
+    price: '65.00',
+    date: new Date('2021-10-21T10:34:23'),
+    currency: 'CAD',
+    category: 'Category #41',
+  },
+  {
+    id: 2,
+    name: 'Coffee',
+    price: '4.29',
+    date: new Date('2021-10-17T10:34:23'),
+    currency: 'CAD',
+    category: 'Category #12',
+  },
+  {
+    id: 3,
+    name: 'Monitor',
+    price: '205.00',
+    date: new Date('2021-09-27T10:34:23'),
+    currency: 'CAD',
+    category: 'Category #13',
+  },
+  {
+    id: 4,
+    name: 'Mouse',
+    price: '54.99',
+    date: new Date('2021-09-05T10:34:23'),
+    currency: 'CAD',
+    category: 'Category #22',
+  },
+  {
+    id: 5,
+    name: 'Skateboard',
+    price: '80.99',
+    date: new Date('2021-08-17T10:34:23'),
+    currency: 'USD',
+    category: 'Category #25',
+  },
 ];
+
+const incomeDatabase = [
+  {
+    id: 6,
+    name: 'Part Time: Piano Lessons',
+    price: '650.00',
+    date: new Date('2021-10-21T10:34:23'),
+    currency: 'CAD',
+    category: 'Category #1',
+  },
+  {
+    id: 7,
+    name: 'Tax Refunds',
+    price: '205.00',
+    date: new Date('2021-09-27T10:34:23'),
+    currency: 'CAD',
+    category: 'Category #13',
+  },
+  {
+    id: 8,
+    name: 'Part Time: CPSC 312 TA',
+    price: '1344.99',
+    date: new Date('2021-09-05T10:34:23'),
+    currency: 'CAD',
+    category: 'Category #1',
+  },
+  {
+    id: 9,
+    name: 'Monthly Allowance',
+    price: '80.99',
+    date: new Date('2021-08-17T10:34:23'),
+    currency: 'USD',
+    category: 'Category #25',
+  },
+];
+
+const getTransactionsToDisplay = () => {
+  /*
+  // sort expenseDatabase and incomeDatabase by Date
+  expenses.sort(function (a, b) {
+    return a.date - b.date;
+  });
+  incomes.sort(function (a, b) {
+    return a.date - b.date;
+  });
+  */
+
+  // then I realized that expenseDatabase and incomeDatabase are already kinda sorted
+  const expenses = expenseDatabase.slice(0, 10);
+  const incomes = incomeDatabase.slice(0, 10);
+
+  // merge two sorted arrays
+  let i = 0,
+    j = 0,
+    k = 0;
+  const temp = [];
+  while (i < expenses.length && j < incomes.length && k < 10) {
+    if (expenses[i].date - incomes[j].date) {
+      temp[k] = expenses[i];
+      i += 1;
+      temp[k].price = '-$'.concat(temp[k].price);
+    } else {
+      temp[k] = incomes[j];
+      j += 1;
+      temp[k].price = '$'.concat(temp[k].price);
+    }
+    k += 1;
+  }
+
+  while (i < expenses.length && k < 10) {
+    temp[k] = expenses[i];
+    i += 1;
+    k += 1;
+  }
+  while (j < incomes.length && k < 10) {
+    temp[k] = incomes[j];
+    j += 1;
+    k += 1;
+  }
+
+  return temp;
+};
 
 const styles = StyleSheet.create({
   centeredView: {
@@ -71,6 +200,8 @@ const styles = StyleSheet.create({
   cardLeft: {
     flex: 1,
     justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    paddingRight: 5,
   },
   cardMid: {
     flex: 1,
@@ -88,7 +219,7 @@ const styles = StyleSheet.create({
     color: '#909090',
   },
   cardText: {
-    fontSize: 18,
+    fontSize: 16,
     fontWeight: '600',
     color: '#24838F',
   },
