f5307c7f74ca46fc4e7fa180aaef83f0511ca661
UBC Student <rafa3l389@gmail.com>
2021-11-26 17:26:55 -0800

    Implemented budget table

---
+++ client/src/components/BudgetPage/BudgetTable.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import { List } from 'react-native-paper';
+import { StyleSheet, ScrollView, SafeAreaView, StatusBar, Text, View } from 'react-native';
+
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+const BudgetTable = (props) => (
+  <BudgetTableHelper renderList={props.renderList}/>
+);
+
+export default BudgetTable;
+
+BudgetTableHelper = (props) => {
+  const componentList = props.renderList.map((budget, index) => (
+    <List.Item  key = {index}
+                // title= {months[budget.month - 1]}
+                    right={() => (
+                      <View style={{flexDirection: 'row'}}>
+                        <View style={{width: 155, height: 20}}>
+                          <Text style = {styles.month}> {months[budget.month - 1]} </Text>
+                        </View>
+                        <View style={{width: 100, height: 20, justifyContent: 'center', alignItems: 'center'}}>
+                          <Text style = {styles.value}> ${budget.value} </Text>
+                        </View>
+                        <View style={{width: 100, height: 20, justifyContent: 'center', alignItems: 'center'}}>
+                          <Text style = {styles.spent}> -${budget.spent} </Text>
+                        </View>
+                      </View>
+                    )}
+                    style={{
+                        borderBottomColor: '#24838f',
+                        borderBottomWidth: 1,
+                        marginBottom: 9,
+                    }}/>
+  ));
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        <List.Section>
+          {componentList}
+        </List.Section>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+  },
+  scrollView: {
+    backgroundColor: 'white',
+    marginHorizontal: 20,
+    borderRadius: 10,
+  },
+  text: {
+    fontSize: 42,
+    margin: 8,
+  },
+  header: {
+    fontSize: 42,
+    margin: 8,
+    backgroundColor: '#24838f',
+    marginHorizontal: 20,
+    borderRadius: 10,
+  },
+  subheader: {
+    color: 'white',
+    fontSize: 20
+  },
+  month:{
+    color: 'black',
+    fontSize: 16
+  },
+  value:{
+    color: 'green',
+    fontSize: 16,
+    textAlign: 'center'
+  },
+  spent:{
+    color: 'red',
+    fontSize: 16,
+    textAlign: 'center'
+  }
+});
--- client/src/pages/BudgetPage.jsx
+++ client/src/pages/BudgetPage.jsx
@@ -1,13 +1,124 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { List, Colors } from 'react-native-paper';
+import { StyleSheet, ScrollView, SafeAreaView, StatusBar } from 'react-native';
+import ActionButton from '../components/ActionButton';
+import BudgetTable from '../components/BudgetPage/BudgetTable';
+
+const budgetDatabase = [
+  {
+    month: 12,
+    year: 2021,
+    value: 500.01,
+    spent: 300.01,
+  },
+  {
+    month: 11,
+    year: 2021,
+    value: 200.27,
+    spent: 100.99,
+  },
+  {
+    month: 9,
+    year: 2021,
+    value: 82.74,
+    spent: 13.78,
+  },
+  {
+    month: 8,
+    year: 2021,
+    value: 98.23,
+    spent: 387.92,
+  },
+  {
+    month: 7,
+    year: 2021,
+    value: 198.63,
+    spent: 96.54,
+  },
+  {
+    month: 6,
+    year: 2021,
+    value: 198.63,
+    spent: 96.54,
+  },
+  {
+    month: 5,
+    year: 2021,
+    value: 198.63,
+    spent: 96.54,
+  },
+  {
+    month: 4,
+    year: 2021,
+    value: 198.63,
+    spent: 96.54,
+  },
+  {
+    month: 3,
+    year: 2021,
+    value: 198.63,
+    spent: 96.54,
+  },
+  {
+    month: 2,
+    year: 2021,
+    value: 198.63,
+    spent: 96.54,
+  }
+];
 
 const BudgetPage = () => {
   return (
-    <View>
-      <Text>Budget Page</Text>
-      <Text>This is the budget page</Text>
-    </View>
+    <>
+    <List.Item title = {<Text style={styles.subheader}>   Month</Text>}
+              style = {styles.header}
+              right = {props => <>
+                                <List.Icon {...props} color = {Colors.green600} icon="arrow-up-bold" />
+                                <Text>              </Text>
+                                <List.Icon {...props} color = {Colors.red600} icon="arrow-down-bold" />
+                                <Text>       </Text>
+                                </>}> </List.Item>
+    <BudgetTable renderList = {budgetDatabase}></BudgetTable>
+    {/* <ActionButton /> */}
+    </>
   );
 };
 
 export default BudgetPage;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+  },
+  scrollView: {
+    backgroundColor: 'white',
+    marginHorizontal: 20,
+    borderRadius: 10,
+  },
+  text: {
+    fontSize: 42,
+    margin: 8,
+  },
+  header: {
+    fontSize: 42,
+    margin: 8,
+    backgroundColor: '#24838f',
+    marginHorizontal: 20,
+    borderRadius: 10,
+  },
+  subheader: {
+    color: 'white',
+    fontSize: 20
+  },
+  value:{
+    color: 'green',
+    fontSize: 16
+  },
+  spent:{
+    color: 'red',
+    fontSize: 16
+  }
+});
+
