dec4f2367d63d5cfb8d56a45b2971c9b7e37d849
Michelle Kim <yeojin011016@gmail.com>
2021-11-26 19:48:53 -0800

    changed styles to match the figma design

--- client/src/components/Home/RecentTransactions.jsx
+++ client/src/components/Home/RecentTransactions.jsx
@@ -5,13 +5,24 @@ const RecentTransactions = () => {
   return (
     <SafeAreaView style={styles.centeredView}>
       <Text style={styles.title}>Recent transactions</Text>
-      <View>
+      <View style={styles.categoryView}>
+        <View style={styles.card}>
+          <Text style={styles.labelText}>Name</Text>
+          <Text style={styles.labelText}>Category</Text>
+          <Text style={styles.labelText}>Amount</Text>
+        </View>
         {sampleData.map((item, index) => {
           return (
-            <View style={styles.card} key={item.name}>
-              <Text style={styles.cardText}>{item.name}</Text>
-              <Text style={styles.cardText}>{item.category}</Text>
-              <Text style={styles.cardText}>{item.amount.toFixed(2)}</Text>
+            <View style={styles.card} key={item.id}>
+              <View style={styles.cardLeft}>
+                <Text style={styles.cardText}>{item.name}</Text>
+              </View>
+              <View style={styles.cardMid}>
+                <Text style={styles.cardText}>{item.category}</Text>
+              </View>
+              <View style={styles.cardRight}>
+                <Text style={styles.cardText}>${item.amount.toFixed(2)}</Text>
+              </View>
             </View>
           );
         })}
@@ -21,12 +32,12 @@ const RecentTransactions = () => {
 };
 
 const sampleData = [
-  { name: 'Expense 1', category: 'Category 1', amount: 25.0 },
-  { name: 'Expense 2', category: 'Category 1', amount: 20.1 },
-  { name: 'Income 1', category: 'Category 2', amount: 250.0 },
-  { name: 'Expense 3', category: 'Category 2', amount: 250.0 },
-  { name: 'Expense 4', category: 'Category 3', amount: 10.75 },
-  { name: 'Expense 5', category: 'Category 1', amount: 20.25 },
+  { id: 1, name: 'Name', category: 'Category 1', amount: 25.0 },
+  { id: 2, name: 'Name', category: 'Category 1', amount: 20.1 },
+  { id: 3, name: 'Name', category: 'Category 2', amount: 250.0 },
+  { id: 4, name: 'Name', category: 'Category 2', amount: 20.0 },
+  { id: 5, name: 'Name', category: 'Category 3', amount: 10.75 },
+  { id: 6, name: 'Name', category: 'Category 1', amount: 20.25 },
 ];
 
 const styles = StyleSheet.create({
@@ -38,23 +49,48 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     borderColor: '#4993ec',
     backgroundColor: 'lightgrey',
+    paddingBottom: 10,
   },
   title: {
     alignSelf: 'center',
     fontSize: 30,
     fontWeight: 'bold',
-    paddingVertical: 20,
+    paddingTop: 10,
+    color: '#24838F',
   },
   card: {
-    paddingVertical: 5,
+    paddingVertical: 10,
     paddingHorizontal: 20,
     marginTop: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: '#24838F75',
+  },
+  cardLeft: {
+    flex: 1,
+    justifyContent: 'flex-start',
+  },
+  cardMid: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cardRight: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+  },
+  labelText: {
+    fontSize: 12,
+    fontWeight: '400',
+    color: '#909090',
   },
   cardText: {
-    fontSize: 16,
+    fontSize: 18,
     fontWeight: '600',
+    color: '#24838F',
   },
 });
 
