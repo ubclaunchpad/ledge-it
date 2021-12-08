298b556d8c9d6ee4d583ebb4402a11c4c0f3684f
William Surya <rafa3l389@gmail.com>
2021-12-07 19:09:21 -0800

    Integrated back end  into Budget Page

--- client/src/components/BudgetPage/BudgetDetails.jsx
+++ client/src/components/BudgetPage/BudgetDetails.jsx
@@ -1,26 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Text, View, Pressable, StyleSheet, SafeAreaView } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import BudgetDetailsTable from './BudgetDetailsTable';
 import { theme } from '../../../theme';
 import { MONTHS } from '../../utils/constants';
-import { getMonth, getYear } from '../../utils/formatters';
 
 const BudgetDetails = ({ expenseDatabase, currentMonth, currentYear, isVisible, setVisible }) => {
-  const [splitList, setSplitList] = useState([]);
-
-  useEffect(() => {
-    const tempList = [];
-    expenseDatabase.forEach((expense) => {
-      const month = MONTHS[getMonth(expense.date) - 1];
-      const year = getYear(expense.date);
-      if (month === currentMonth && year === currentYear) {
-        tempList.push(expense);
-      }
-    });
-    setSplitList(tempList);
-  }, [expenseDatabase, currentMonth, currentYear]);
-
   return (
     <SafeAreaView style={styles.listContainer}>
       <View style={styles.header}>
@@ -28,11 +13,11 @@ const BudgetDetails = ({ expenseDatabase, currentMonth, currentYear, isVisible, 
           <Ionicons name="arrow-back-outline" color={theme.colors.primaryDark} size={35} />
         </Pressable>
         <View style={{ alignItems: 'center', marginBottom: 10 }}>
-          <Text style={styles.title}>{currentMonth}</Text>
+          <Text style={styles.title}>{MONTHS[currentMonth-1]}</Text>
         </View>
       </View>
-      {splitList.length > 0 ? (
-        <BudgetDetailsTable renderList={splitList} />
+      {expenseDatabase.length > 0 ? (
+        <BudgetDetailsTable renderList={expenseDatabase} />
       ) : (
         <View>
           <Text style={styles.message}>No expenses in this month</Text>
--- client/src/pages/BudgetPage.jsx
+++ client/src/pages/BudgetPage.jsx
@@ -1,157 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import BudgetTable from '../components/BudgetPage/BudgetTable';
 import BudgetDetails from '../components/BudgetPage/BudgetDetails';
-
-const budgetDatabase = [
-  {
-    month: 11,
-    year: 2021,
-    value: 200.27,
-    spent: 100.99,
-  },
-  {
-    month: 9,
-    year: 2021,
-    value: 82.74,
-    spent: 13.78,
-  },
-  {
-    month: 8,
-    year: 2021,
-    value: 98.23,
-    spent: 387.92,
-  },
-  {
-    month: 7,
-    year: 2021,
-    value: 198.63,
-    spent: 96.54,
-  },
-  {
-    month: 6,
-    year: 2021,
-    value: 198.63,
-    spent: 96.54,
-  },
-  {
-    month: 5,
-    year: 2021,
-    value: 198.63,
-    spent: 96.54,
-  },
-  {
-    month: 4,
-    year: 2021,
-    value: 198.63,
-    spent: 96.54,
-  },
-  {
-    month: 3,
-    year: 2021,
-    value: 198.63,
-    spent: 96.54,
-  },
-  {
-    month: 2,
-    year: 2021,
-    value: 198.63,
-    spent: 96.54,
-  },
-  {
-    month: 0,
-    year: 2021,
-    value: 1098.63,
-    spent: 906.54,
-  },
-  {
-    month: 11,
-    year: 2020,
-    value: 1098.63,
-    spent: 906.54,
-  },
-  {
-    month: 10,
-    year: 2020,
-    value: 1098.63,
-    spent: 906.54,
-  },
-  {
-    month: 0,
-    year: 2019,
-    value: 1098.63,
-    spent: 906.54,
-  },
-];
-
-const expenseDatabase = [
-  {
-    name: 'Spotify',
-    price: '5.99',
-    date: '2021-12-17',
-    currency: 'USD',
-    category: 'Category #25',
-  },
-  {
-    name: 'Gym',
-    price: '65.00',
-    date: '2021-12-16',
-    currency: 'CAD',
-    category: 'Category #41',
-  },
-  {
-    name: 'Gym',
-    price: '65.00',
-    date: '2021-12-12',
-    currency: 'CAD',
-    category: 'Category #41',
-  },
-  {
-    name: 'Coffee',
-    price: '4.29',
-    date: '2021-12-10',
-    currency: 'CAD',
-    category: 'Category #12',
-  },
-  {
-    name: 'Monitor',
-    price: '205.00',
-    date: '2021-12-9',
-    currency: 'CAD',
-    category: 'Category #13',
-  },
-  {
-    name: 'Mouse',
-    price: '54.99',
-    date: '2021-08-17',
-    currency: 'CAD',
-    category: 'Category #22',
-  },
-  {
-    name: 'Skateboard',
-    price: '80.99',
-    date: '2021-08-01',
-    currency: 'USD',
-    category: 'Category #25',
-  },
-  {
-    name: 'Spotify',
-    price: '5.99',
-    date: '2020-12-17',
-    currency: 'USD',
-    category: 'Category #25',
-  },
-];
+import axios from 'axios';
 
 const BudgetPage = () => {
   const [showDetails, setShowDetails] = useState(false);
   const [month, setMonth] = useState(new Date().getMonth() + 1);
   const [year, setYear] = useState(new Date().getFullYear());
+  const [databaseBudget, setDatabaseBudget] = useState([]);
+  const [databaseExpense, setDatabaseExpense] = useState([]);
+
+  useEffect (() => {
+    var tempList = [];
+    var tempList2 = [];
+    console.log('https://money-manager-dev.herokuapp.com/expense/' + year + '/'+ month)
+    axios
+      .get('https://money-manager-dev.herokuapp.com/budget/all')
+      .then((response) => {
+        response.data.forEach((item) => {
+          tempList.push(item);
+        });
+        setDatabaseBudget(tempList);
+      })
+      .catch((err) => {
+        setDatabaseBudget(tempList);
+        console.log(err)
+      });
+
+    axios
+      .get('https://money-manager-dev.herokuapp.com/expense/' + year + '/'+ month)
+      .then((response) => {
+        response.data.forEach((item) => {
+          tempList2.push(item);
+        });
+        setDatabaseExpense(tempList2);
+      })
+      .catch((err) => {
+        setDatabaseExpense(tempList2);
+        console.log(err)
+      });
+  }, [showDetails]);
 
   return (
     <>
       {showDetails ? (
         <BudgetDetails
-          expenseDatabase={expenseDatabase}
+          expenseDatabase={databaseExpense}
           currentMonth={month}
           currentYear={year}
           isVisible={showDetails}
@@ -159,7 +53,7 @@ const BudgetPage = () => {
         />
       ) : (
         <BudgetTable
-          renderList={budgetDatabase}
+          renderList={databaseBudget}
           isVisible={showDetails}
           setVisible={setShowDetails}
           setMonth={setMonth}
