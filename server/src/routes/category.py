591acf9461859fdfc4835444e043642c341da024
Mathew Bushuru <bushuruwafulamathew@gmail.com>
2022-02-12 11:01:56 -0800

    added category delete endpoint-2

--- server/src/routes/category.py
+++ server/src/routes/category.py
@@ -13,101 +13,67 @@ router = APIRouter()
     "/expense_categories",
     response_description="Get user's expense categories",
 )
-# async def get_current_user_expense_categories(current_user: User = Depends(get_current_active_user)):
-#     current_user_expense_categories = current_user.expense_categories_list
-#     if current_user_expense_categories is not None:
-#         return current_user_expense_categories
-#     raise HTTPException(
-#         status_code=404, detail=f"Categories for current user not found"
-#     )
-
-# For some reason, i cant get user from db, using this function below instead for testing.
-# If it works, use above function instead which makes sure user is logged in before making request
-async def get_current_user_expense_categories():
-    data = {
-        "_id": "61fedf8e1986effc5354b6c5",
-        "created_at": "2022-02-05T19:56:27.942804",
-        "updated_at": "2022-02-05T19:56:27.942854",
-        "email": "gregork@ubc.com",
-        "hashed_password": pwd_context.hash("gregor"),
-        "active": True,
-        "expense_categories_list": [
-            {
-                "_id": "71fedf8e1986effc5354b6c5",
-                "created_at": "2022-02-05T17:46:14.543981",
-                "updated_at": "2022-02-05T17:46:14.543981",
-                "name": "Housing",
-                "color": "blue",
-            },
-            {
-                "_id": "81fedf8e1986effc5354b6c5",
-                "created_at": "2022-02-05T17:46:14.543981",
-                "updated_at": "2022-02-05T17:46:14.543981",
-                "name": "Food",
-                "color": "green",
-            },
-        ],
-        "income_categories_list": [
-            {
-                "_id": "71fedf8e1986effc5354b6c5",
-                "created_at": "2022-02-05T17:46:14.543981",
-                "updated_at": "2022-02-05T17:46:14.543981",
-                "name": "Salary",
-                "color": "purple",
-            },
-        ],
-    }
-    current_user = User(**data)
-    if current_user is None:
-        raise HTTPException(status_code=404, detail=f"User not found")
+async def get_current_user_expense_categories(current_user: User = Depends(get_current_active_user)):
     current_user_expense_categories = current_user.expense_categories_list
     if current_user_expense_categories is not None:
         return current_user_expense_categories
-    else:
-        raise HTTPException(
-            status_code=404, detail=f"Categories for current user not found"
-        )
+    raise HTTPException(
+        status_code=404, detail=f"Categories for current user not found"
+    )
+
+# async def get_current_user_expense_categories():
+#     data = {
+#         "_id": "61fedf8e1986effc5354b6c5",
+#         "created_at": "2022-02-05T19:56:27.942804",
+#         "updated_at": "2022-02-05T19:56:27.942854",
+#         "email": "gregork@ubc.com",
+#         "hashed_password": pwd_context.hash("gregor"),
+#         "active": True,
+#         "expense_categories_list": [
+#             {
+#                 "_id": "71fedf8e1986effc5354b6c5",
+#                 "created_at": "2022-02-05T17:46:14.543981",
+#                 "updated_at": "2022-02-05T17:46:14.543981",
+#                 "name": "Housing",
+#                 "color": "blue",
+#             },
+#             {
+#                 "_id": "81fedf8e1986effc5354b6c5",
+#                 "created_at": "2022-02-05T17:46:14.543981",
+#                 "updated_at": "2022-02-05T17:46:14.543981",
+#                 "name": "Food",
+#                 "color": "green",
+#             },
+#         ],
+#         "income_categories_list": [
+#             {
+#                 "_id": "71fedf8e1986effc5354b6c5",
+#                 "created_at": "2022-02-05T17:46:14.543981",
+#                 "updated_at": "2022-02-05T17:46:14.543981",
+#                 "name": "Salary",
+#                 "color": "purple",
+#             },
+#         ],
+#     }
+#     current_user = User(**data)
+#     if current_user is None:
+#         raise HTTPException(status_code=404, detail=f"User not found")
+#     current_user_expense_categories = current_user.expense_categories_list
+#     if current_user_expense_categories is not None:
+#         return current_user_expense_categories
+#     else:
+#         raise HTTPException(
+#             status_code=404, detail=f"Categories for current user not found"
+#         )
 
 
 @router.delete(
-    "/expense_categories/{id}",
-    response_description="Delete category",
+    "/delete_expense_categories/{id}",
+    response_description="Delete expense category",
 )
 async def delete_expense_categories_by_id(user_id: str, category_id: str):
-    data = {
-        "_id": "61fedf8e1986effc5354b6c5",
-        "created_at": "2022-02-05T19:56:27.942804",
-        "updated_at": "2022-02-05T19:56:27.942854",
-        "email": "gregork@ubc.com",
-        "hashed_password": pwd_context.hash("gregor"),
-        "active": True,
-        "expense_categories_list": [
-            {
-                "_id": "61fedf8e1986effc5354b6c5",
-                "created_at": "2022-02-05T17:46:14.543981",
-                "updated_at": "2022-02-05T17:46:14.543981",
-                "name": "Housing",
-                "color": "blue",
-            },
-            {
-                "_id": "81fedf8e1986effc5354b6c5",
-                "created_at": "2022-02-05T17:46:14.543981",
-                "updated_at": "2022-02-05T17:46:14.543981",
-                "name": "Food",
-                "color": "green",
-            },
-        ],
-        "income_categories_list": [
-            {
-                "_id": "71fedf8e1986effc5354b6c5",
-                "created_at": "2022-02-05T17:46:14.543981",
-                "updated_at": "2022-02-05T17:46:14.543981",
-                "name": "Salary",
-                "color": "purple",
-            },
-        ],
-    }
-    current_user = User(**data)
+    
+    current_user: User = Depends(get_current_active_user)
     if current_user is None:
         raise HTTPException(status_code=404, detail=f"User not found")
     current_user_expense_categories = current_user.expense_categories_list
@@ -122,6 +88,59 @@ async def delete_expense_categories_by_id(user_id: str, category_id: str):
             status_code=404, detail=f"Categories for current user not found"
         )
 
+# @router.delete(
+#     "/delete_expense_categories/{id}",
+#     response_description="Delete expense category",
+# )
+# async def delete_expense_categories_by_id(user_id: str, category_id: str):
+#     data = {
+#         "_id": "61fedf8e1986effc5354b6c5",
+#         "created_at": "2022-02-05T19:56:27.942804",
+#         "updated_at": "2022-02-05T19:56:27.942854",
+#         "email": "gregork@ubc.com",
+#         "hashed_password": pwd_context.hash("gregor"),
+#         "active": True,
+#         "expense_categories_list": [
+#             {
+#                 "_id": "61fedf8e1986effc5354b6c5",
+#                 "created_at": "2022-02-05T17:46:14.543981",
+#                 "updated_at": "2022-02-05T17:46:14.543981",
+#                 "name": "Housing",
+#                 "color": "blue",
+#             },
+#             {
+#                 "_id": "81fedf8e1986effc5354b6c5",
+#                 "created_at": "2022-02-05T17:46:14.543981",
+#                 "updated_at": "2022-02-05T17:46:14.543981",
+#                 "name": "Food",
+#                 "color": "green",
+#             },
+#         ],
+#         "income_categories_list": [
+#             {
+#                 "_id": "71fedf8e1986effc5354b6c5",
+#                 "created_at": "2022-02-05T17:46:14.543981",
+#                 "updated_at": "2022-02-05T17:46:14.543981",
+#                 "name": "Salary",
+#                 "color": "purple",
+#             },
+#         ],
+#     }
+#     current_user = User(**data)
+#     if current_user is None:
+#         raise HTTPException(status_code=404, detail=f"User not found")
+#     current_user_expense_categories = current_user.expense_categories_list
+#     if current_user_expense_categories is not None:
+#         user_collection.update_one(
+#             {"_id": user_id},
+#             {"$pull": {"current_user.expense_categories_list": {"_id": category_id}}},
+#         )
+#         return current_user_expense_categories
+#     else:
+#         raise HTTPException(
+#             status_code=404, detail=f"Categories for current user not found"
+#         )
+
 
 @router.get(
     "/income_categories",
@@ -135,6 +154,26 @@ async def get_current_user_income_categories(
     current_user_income_categories = current_user.income_categories_list
     if current_user_income_categories is not None:
         return current_user_income_categories
+    else:
+        raise HTTPException(
+            status_code=404, detail=f"Categories for current user not found"
+        )
+
+@router.delete(
+    "/delete_income_categories/{id}",
+    response_description="Delete income category",
+)
+async def delete_expense_categories_by_id(user_id: str, category_id: str):
+    current_user: User = Depends(get_current_active_user)
+    if current_user is None:
+        raise HTTPException(status_code=404, detail=f"User not found")
+    current_user_expense_categories = current_user.expense_categories_list
+    if current_user_expense_categories is not None:
+        user_collection.update_one(
+            {"_id": user_id},
+            {"$pull": {"current_user.expense_categories_list": {"_id": category_id}}},
+        )
+        return current_user_expense_categories
     else:
         raise HTTPException(
             status_code=404, detail=f"Categories for current user not found"
