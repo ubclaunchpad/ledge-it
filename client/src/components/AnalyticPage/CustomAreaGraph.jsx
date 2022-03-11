b8f3c15d5b03a63e0057f6c95d722aef56e70643
William Suryawidjaja <rafa3l389@gmail.com>
2022-03-10 22:42:36 -0800

    Ran linter

--- client/src/components/AnalyticPage/CustomAreaGraph.jsx
+++ client/src/components/AnalyticPage/CustomAreaGraph.jsx
@@ -1,57 +1,57 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { Button, DataTable } from 'react-native-paper';
-import * as shape from 'd3-shape' 
+import * as shape from 'd3-shape';
 import {
   SlideAreaChart,
   CursorProps,
   ToolTipProps,
   ToolTipTextRenderersInput,
-  GradientProps
-} from '@connectedcars/react-native-slide-charts'
+  GradientProps,
+} from '@connectedcars/react-native-slide-charts';
 
-import { Stop, LinearGradient } from 'react-native-svg'
+import { Stop, LinearGradient } from 'react-native-svg';
 
-let priceToDisplay = "";
-let earliestExpense = new Date(); 
+let priceToDisplay = '';
+let earliestExpense = new Date();
 
 const CustomAreaGraph = ({ dateStringData, dateData }) => {
   const [dateRange, setDateRange] = useState(null);
-  const [dateHeader, setDateHeader] = useState("All Time");
+  const [dateHeader, setDateHeader] = useState('All Time');
   const [priceHeader, setPriceHeader] = useState(0);
-  const [selectedRange, setSelectedRange] = useState("All");
+  const [selectedRange, setSelectedRange] = useState('All');
 
   const calculateEarliest = (data) => {
-    if (data.length > 0){
-      earliestExpense = new Date(data[0]["x"])
+    if (data.length > 0) {
+      earliestExpense = new Date(data[0].x);
     }
-  }
+  };
 
   const calculateTotal = (data) => {
     let total = 0;
     data.forEach((row) => {
-      total = total + Number(row["y"]);
-    })
+      total += Number(row.y);
+    });
     total = total.toFixed(2);
     setPriceHeader(total);
-  }
+  };
 
   const handleRangeChange = (newBounds) => {
     const today = new Date();
     let lowerBound = new Date();
     setSelectedRange(newBounds);
-    if (newBounds === "1W"){
-      lowerBound = new Date(today.getFullYear(), today.getMonth(), today.getDate()-7);
-    } else if (newBounds === "1M"){
-      lowerBound = new Date(today.getFullYear(), today.getMonth()-1, today.getDate());
-    } else if (newBounds === "3M") {
-      lowerBound = new Date(today.getFullYear(), today.getMonth()-3, today.getDate())
-    } else if (newBounds === "1Y") {
-      lowerBound = new Date(today.getFullYear()-1, today.getMonth(), today.getDate())
-    } else if (newBounds === "2Y") {
-      lowerBound = new Date(today.getFullYear()-2, today.getMonth(), today.getDate())
-    } else if (newBounds === "All"){
-      setDateHeader("All Time")
+    if (newBounds === '1W') {
+      lowerBound = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
+    } else if (newBounds === '1M') {
+      lowerBound = new Date(today.getFullYear(), today.getMonth() - 1, today.getDate());
+    } else if (newBounds === '3M') {
+      lowerBound = new Date(today.getFullYear(), today.getMonth() - 3, today.getDate());
+    } else if (newBounds === '1Y') {
+      lowerBound = new Date(today.getFullYear() - 1, today.getMonth(), today.getDate());
+    } else if (newBounds === '2Y') {
+      lowerBound = new Date(today.getFullYear() - 2, today.getMonth(), today.getDate());
+    } else if (newBounds === 'All') {
+      setDateHeader('All Time');
       setDateRange(null);
       calculateTotal(dateData);
       return;
@@ -61,150 +61,178 @@ const CustomAreaGraph = ({ dateStringData, dateData }) => {
     // To improve performance, only take the bound lower if there is an expense that exists beyond that value.
     // Otherwise, set the lower bound as the first expense.
     lowerBound = new Date(Math.max(lowerBound, earliestExpense));
-    var options = { year: 'numeric', month: 'long', day: 'numeric' };
-    let todayString = today.toLocaleDateString("en-US", options);
-    let lowerBoundString = lowerBound.toLocaleDateString("en-US", options);
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    const todayString = today.toLocaleDateString('en-US', options);
+    const lowerBoundString = lowerBound.toLocaleDateString('en-US', options);
 
-    if (lowerBoundString !== todayString){
-      setDateHeader(lowerBoundString + "-" + todayString);
+    if (lowerBoundString !== todayString) {
+      setDateHeader(`${lowerBoundString}-${todayString}`);
     } else {
       setDateHeader(todayString);
     }
     setDateRange([lowerBound, today]);
 
-    let filteredData = dateData.filter((row) => 
-      row["x"] >= lowerBound
-    )
-    
+    const filteredData = dateData.filter((row) => row.x >= lowerBound);
+
     calculateTotal(filteredData);
-  }
+  };
 
   const displayPrice = (scaleX, x, scaleY, y) => {
-    let date = scaleX.invert(x).toISOString().split('T')[0];
-    let data = dateStringData.find((obj) => obj.x === date);
-    if (data !== undefined){
-      priceToDisplay = "$" + data.y.toString()
+    const date = scaleX.invert(x).toISOString().split('T')[0];
+    const data = dateStringData.find((obj) => obj.x === date);
+    if (data !== undefined) {
+      priceToDisplay = `$${data.y.toString()}`;
     } else {
-      priceToDisplay = "N/A"
+      priceToDisplay = 'N/A';
     }
     return priceToDisplay;
-  }
+  };
 
   const customAreaChartFillGradient = (props) => {
     return (
-      <LinearGradient x1='0%' y1='0%' x2='0%' y2='100%' {...props}>
-        <Stop stopColor='white' offset='20%' stopOpacity='0.9' />
-        <Stop stopColor='#24838F' offset='100%' stopOpacity='0.2' />
+      <LinearGradient x1="0%" y1="0%" x2="0%" y2="100%" {...props}>
+        <Stop stopColor="white" offset="20%" stopOpacity="0.9" />
+        <Stop stopColor="#24838F" offset="100%" stopOpacity="0.2" />
       </LinearGradient>
-    )
-  }
+    );
+  };
 
   useEffect(() => {
     calculateEarliest(dateData);
     calculateTotal(dateData);
-  }, [dateData])
+  }, [dateData]);
 
   return (
     <View style={styles.container}>
       <View style={styles.container}>
         <Text style={styles.priceHeader}> - ${priceHeader}</Text>
-        <Text style={styles.dateHeader}>{dateHeader}</Text> 
+        <Text style={styles.dateHeader}>{dateHeader}</Text>
         <SlideAreaChart
-            scrollable
-            style={{ backgroundColor: "#24838F"}}
-            shouldCancelWhenOutside={false}yo
-            data={dateData}
-            height = {325}
-            axisWidth={0}
-            axisHeight={0}
-            animated = {false}
-            paddingBottom={8}
-            chartLineColor = "#195D66"
-            chartLineWidth={2}
-            cursorProps = {{
-              cursorBorderColor: "#808080",
-              cursorColor: '#808080',
-              cursorLine: false,
-              cursorMarkerHeight: 12,
-              cursorMarkerWidth: 12
-            }
-            }
-            chartPaddingTop = {80}
-            xScale = "time"
-            xRange = {dateRange}
-            yAxisProps={{
-              horizontalLineWidth: 0,
-              verticalLineWidth: 0,
-              axisLabel: 'Y Units',
-              axisLabelAlignment: 'middle',
-              rotateAxisLabel: true,
-              numberOfTicks: 2,
-              hideMarkers: true,
-            }}
-            xAxisProps={{
-              axisLabel: 'X Units',
-            }}
-            renderFillGradient = {e => customAreaChartFillGradient(e)}
-            curveType = {shape.curveBumpX}
-            toolTipProps={{
-              toolTipTextRenderers: [
-                ({ scaleX, x, scaleY, y }) => ({
-                  text: displayPrice(scaleX, x, scaleY, y)
-                }),
-                ({ scaleX, x }) => ({
-                  text: scaleX.invert(x).toISOString().split('T')[0]
-                }),
-              ],
-            }}
-          />
-        <DataTable style = {{backgroundColor: "#24838F", paddingBottom: 10}}>
+          scrollable
+          style={{ backgroundColor: '#24838F' }}
+          shouldCancelWhenOutside={false}
+          yo
+          data={dateData}
+          height={325}
+          axisWidth={0}
+          axisHeight={0}
+          animated={false}
+          paddingBottom={8}
+          chartLineColor="#195D66"
+          chartLineWidth={2}
+          cursorProps={{
+            cursorBorderColor: '#808080',
+            cursorColor: '#808080',
+            cursorLine: false,
+            cursorMarkerHeight: 12,
+            cursorMarkerWidth: 12,
+          }}
+          chartPaddingTop={80}
+          xScale="time"
+          xRange={dateRange}
+          yAxisProps={{
+            horizontalLineWidth: 0,
+            verticalLineWidth: 0,
+            axisLabel: 'Y Units',
+            axisLabelAlignment: 'middle',
+            rotateAxisLabel: true,
+            numberOfTicks: 2,
+            hideMarkers: true,
+          }}
+          xAxisProps={{
+            axisLabel: 'X Units',
+          }}
+          renderFillGradient={(e) => customAreaChartFillGradient(e)}
+          curveType={shape.curveBumpX}
+          toolTipProps={{
+            toolTipTextRenderers: [
+              ({ scaleX, x, scaleY, y }) => ({
+                text: displayPrice(scaleX, x, scaleY, y),
+              }),
+              ({ scaleX, x }) => ({
+                text: scaleX.invert(x).toISOString().split('T')[0],
+              }),
+            ],
+          }}
+        />
+        <DataTable style={{ backgroundColor: '#24838F', paddingBottom: 10 }}>
           <DataTable.Row>
             <DataTable.Cell numeric>
-              <Button 
-                style = {{backgroundColor: selectedRange === "1W" ? "#00ABF0" : "#24838F", borderRadius: 20}}
-                color='white'
-                onPress = {() => {handleRangeChange("1W")}}>
+              <Button
+                style={{
+                  backgroundColor: selectedRange === '1W' ? '#00ABF0' : '#24838F',
+                  borderRadius: 20,
+                }}
+                color="white"
+                onPress={() => {
+                  handleRangeChange('1W');
+                }}>
                 1W
               </Button>
             </DataTable.Cell>
             <DataTable.Cell numeric>
               <Button
-                style = {{backgroundColor: selectedRange === "1M" ? "#00ABF0" : "#24838F", borderRadius: 20}}
-                color='white'
-                onPress = {() => {handleRangeChange("1M")}}>
+                style={{
+                  backgroundColor: selectedRange === '1M' ? '#00ABF0' : '#24838F',
+                  borderRadius: 20,
+                }}
+                color="white"
+                onPress={() => {
+                  handleRangeChange('1M');
+                }}>
                 1M
               </Button>
             </DataTable.Cell>
             <DataTable.Cell numeric>
               <Button
-                style = {{backgroundColor: selectedRange === "3M" ? "#00ABF0" : "#24838F", borderRadius: 20}}
-                color='white'
-                onPress = {() => {handleRangeChange("3M")}}>
+                style={{
+                  backgroundColor: selectedRange === '3M' ? '#00ABF0' : '#24838F',
+                  borderRadius: 20,
+                }}
+                color="white"
+                onPress={() => {
+                  handleRangeChange('3M');
+                }}>
                 3M
               </Button>
             </DataTable.Cell>
             <DataTable.Cell numeric>
               <Button
-                style = {{backgroundColor: selectedRange === "1Y" ? "#00ABF0" : "#24838F", borderRadius: 20}}
-                color='white'
-                onPress = {() => {handleRangeChange("1Y")}}>
+                style={{
+                  backgroundColor: selectedRange === '1Y' ? '#00ABF0' : '#24838F',
+                  borderRadius: 20,
+                }}
+                color="white"
+                onPress={() => {
+                  handleRangeChange('1Y');
+                }}>
                 1Y
               </Button>
             </DataTable.Cell>
             <DataTable.Cell numeric>
               <Button
-                style = {{backgroundColor: selectedRange === "2Y" ? "#00ABF0" : "#24838F", borderRadius: 20}}
-                color='white'
-                onPress = {() => {handleRangeChange("2Y")}}>
+                style={{
+                  backgroundColor: selectedRange === '2Y' ? '#00ABF0' : '#24838F',
+                  borderRadius: 20,
+                }}
+                color="white"
+                onPress={() => {
+                  handleRangeChange('2Y');
+                }}>
                 2Y
               </Button>
             </DataTable.Cell>
             <DataTable.Cell numeric>
               <Button
-                style = {{backgroundColor: selectedRange === "All" ? "#00ABF0" : "#24838F", borderRadius: 20}}
-                color='white'
-                onPress = {() => {handleRangeChange("All")}}>
-                All 
+                style={{
+                  backgroundColor: selectedRange === 'All' ? '#00ABF0' : '#24838F',
+                  borderRadius: 20,
+                }}
+                color="white"
+                onPress={() => {
+                  handleRangeChange('All');
+                }}>
+                All
               </Button>
             </DataTable.Cell>
           </DataTable.Row>
@@ -223,20 +251,19 @@ const styles = StyleSheet.create({
     flexGrow: 1,
   },
   priceHeader: {
-    backgroundColor: "#24838F",
+    backgroundColor: '#24838F',
     fontSize: 36,
     fontWeight: '600',
     color: 'white',
     textAlign: 'center',
-    paddingTop: 60
+    paddingTop: 60,
   },
   dateHeader: {
-    backgroundColor: "#24838F",
+    backgroundColor: '#24838F',
     justifyContent: 'center',
     fontSize: 18,
     fontWeight: '600',
     color: 'white',
-    textAlign: 'center'
+    textAlign: 'center',
   },
 });
-
--- client/src/pages/AnalyticsPage.jsx
+++ client/src/pages/AnalyticsPage.jsx
@@ -1,220 +1,219 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
 import CustomAreaGraph from '../components/AnalyticPage/CustomAreaGraph';
- 
+
 const unprocessedData = [
   {
-    "_id": "6222fb24ad4f0b119bedcaf2",
-    "created_at": "2022-03-05T05:48:13.190430",
-    "updated_at": "2022-03-05T05:48:13.190498",
-    "email": "ledge@gmail.com",
-    "name": "Test Expense 3",
-    "description": "Testing 3",
-    "date": "2022-02-01",
-    "price": 140.0,
-    "currency": "CAD",
-    "exchange_rate": 1.0,
-    "location": null,
-    "location_x": null,
-    "location_y": null,
-    "category": "Food",
-    "sub_category": null
-},
-{
-  "_id": "6222fb24ad4f0b119bedcaf2",
-  "created_at": "2022-03-05T05:48:13.190430",
-  "updated_at": "2022-03-05T05:48:13.190498",
-  "email": "ledge@gmail.com",
-  "name": "Test Expense 3",
-  "description": "Testing 3",
-  "date": "2022-02-03",
-  "price": 67.8,
-  "currency": "CAD",
-  "exchange_rate": 1.0,
-  "location": null,
-  "location_x": null,
-  "location_y": null,
-  "category": "Food",
-  "sub_category": null
-},
-{
-  "_id": "6222fb24ad4f0b119bedcaf2",
-  "created_at": "2022-03-05T05:48:13.190430",
-  "updated_at": "2022-03-05T05:48:13.190498",
-  "email": "ledge@gmail.com",
-  "name": "Test Expense 3",
-  "description": "Testing 3",
-  "date": "2022-02-03",
-  "price": 167.8,
-  "currency": "CAD",
-  "exchange_rate": 1.0,
-  "location": null,
-  "location_x": null,
-  "location_y": null,
-  "category": "Food",
-  "sub_category": null
-},
-{
-  "_id": "6222fb24ad4f0b119bedcaf2",
-  "created_at": "2022-03-05T05:48:13.190430",
-  "updated_at": "2022-03-05T05:48:13.190498",
-  "email": "ledge@gmail.com",
-  "name": "Test Expense 3",
-  "description": "Testing 3",
-  "date": "2022-02-06",
-  "price": 300.0,
-  "currency": "CAD",
-  "exchange_rate": 1.0,
-  "location": null,
-  "location_x": null,
-  "location_y": null,
-  "category": "Food",
-  "sub_category": null
-},
-{
-  "_id": "6222fb24ad4f0b119bedcaf2",
-  "created_at": "2022-03-05T05:48:13.190430",
-  "updated_at": "2022-03-05T05:48:13.190498",
-  "email": "ledge@gmail.com",
-  "name": "Test Expense 3",
-  "description": "Testing 3",
-  "date": "2022-02-06",
-  "price": 310.1,
-  "currency": "CAD",
-  "exchange_rate": 1.0,
-  "location": null,
-  "location_x": null,
-  "location_y": null,
-  "category": "Food",
-  "sub_category": null
-},
-{
-  "_id": "6222fb24ad4f0b119bedcaf2",
-  "created_at": "2022-03-05T05:48:13.190430",
-  "updated_at": "2022-03-05T05:48:13.190498",
-  "email": "ledge@gmail.com",
-  "name": "Test Expense 3",
-  "description": "Testing 3",
-  "date": "2022-02-10",
-  "price": 200.0,
-  "currency": "CAD",
-  "exchange_rate": 1.0,
-  "location": null,
-  "location_x": null,
-  "location_y": null,
-  "category": "Food",
-  "sub_category": null
-},
-{
-  "_id": "6222fb24ad4f0b119bedcaf2",
-  "created_at": "2022-03-05T05:48:13.190430",
-  "updated_at": "2022-03-05T05:48:13.190498",
-  "email": "ledge@gmail.com",
-  "name": "Test Expense 3",
-  "description": "Testing 3",
-  "date": "2022-02-10",
-  "price": 200.0,
-  "currency": "CAD",
-  "exchange_rate": 1.0,
-  "location": null,
-  "location_x": null,
-  "location_y": null,
-  "category": "Food",
-  "sub_category": null
-}, 
-{
-  "_id": "6222fb24ad4f0b119bedcaf2",
-  "created_at": "2022-03-05T05:48:13.190430",
-  "updated_at": "2022-03-05T05:48:13.190498",
-  "email": "ledge@gmail.com",
-  "name": "Test Expense 3",
-  "description": "Testing 3",
-  "date": "2022-03-02",
-  "price": 435.46,
-  "currency": "CAD",
-  "exchange_rate": 1.0,
-  "location": null,
-  "location_x": null,
-  "location_y": null,
-  "category": "Food",
-  "sub_category": null
-}, 
-{
-  "_id": "6222fb24ad4f0b119bedcaf2",
-  "created_at": "2022-03-05T05:48:13.190430",
-  "updated_at": "2022-03-05T05:48:13.190498",
-  "email": "ledge@gmail.com",
-  "name": "Test Expense 3",
-  "description": "Testing 3",
-  "date": "2022-03-07",
-  "price": 143.46,
-  "currency": "CAD",
-  "exchange_rate": 1.0,
-  "location": null,
-  "location_x": null,
-  "location_y": null,
-  "category": "Food",
-  "sub_category": null
-}, 
-]
+    _id: '6222fb24ad4f0b119bedcaf2',
+    created_at: '2022-03-05T05:48:13.190430',
+    updated_at: '2022-03-05T05:48:13.190498',
+    email: 'ledge@gmail.com',
+    name: 'Test Expense 3',
+    description: 'Testing 3',
+    date: '2022-02-01',
+    price: 140.0,
+    currency: 'CAD',
+    exchange_rate: 1.0,
+    location: null,
+    location_x: null,
+    location_y: null,
+    category: 'Food',
+    sub_category: null,
+  },
+  {
+    _id: '6222fb24ad4f0b119bedcaf2',
+    created_at: '2022-03-05T05:48:13.190430',
+    updated_at: '2022-03-05T05:48:13.190498',
+    email: 'ledge@gmail.com',
+    name: 'Test Expense 3',
+    description: 'Testing 3',
+    date: '2022-02-03',
+    price: 67.8,
+    currency: 'CAD',
+    exchange_rate: 1.0,
+    location: null,
+    location_x: null,
+    location_y: null,
+    category: 'Food',
+    sub_category: null,
+  },
+  {
+    _id: '6222fb24ad4f0b119bedcaf2',
+    created_at: '2022-03-05T05:48:13.190430',
+    updated_at: '2022-03-05T05:48:13.190498',
+    email: 'ledge@gmail.com',
+    name: 'Test Expense 3',
+    description: 'Testing 3',
+    date: '2022-02-03',
+    price: 167.8,
+    currency: 'CAD',
+    exchange_rate: 1.0,
+    location: null,
+    location_x: null,
+    location_y: null,
+    category: 'Food',
+    sub_category: null,
+  },
+  {
+    _id: '6222fb24ad4f0b119bedcaf2',
+    created_at: '2022-03-05T05:48:13.190430',
+    updated_at: '2022-03-05T05:48:13.190498',
+    email: 'ledge@gmail.com',
+    name: 'Test Expense 3',
+    description: 'Testing 3',
+    date: '2022-02-06',
+    price: 300.0,
+    currency: 'CAD',
+    exchange_rate: 1.0,
+    location: null,
+    location_x: null,
+    location_y: null,
+    category: 'Food',
+    sub_category: null,
+  },
+  {
+    _id: '6222fb24ad4f0b119bedcaf2',
+    created_at: '2022-03-05T05:48:13.190430',
+    updated_at: '2022-03-05T05:48:13.190498',
+    email: 'ledge@gmail.com',
+    name: 'Test Expense 3',
+    description: 'Testing 3',
+    date: '2022-02-06',
+    price: 310.1,
+    currency: 'CAD',
+    exchange_rate: 1.0,
+    location: null,
+    location_x: null,
+    location_y: null,
+    category: 'Food',
+    sub_category: null,
+  },
+  {
+    _id: '6222fb24ad4f0b119bedcaf2',
+    created_at: '2022-03-05T05:48:13.190430',
+    updated_at: '2022-03-05T05:48:13.190498',
+    email: 'ledge@gmail.com',
+    name: 'Test Expense 3',
+    description: 'Testing 3',
+    date: '2022-02-10',
+    price: 200.0,
+    currency: 'CAD',
+    exchange_rate: 1.0,
+    location: null,
+    location_x: null,
+    location_y: null,
+    category: 'Food',
+    sub_category: null,
+  },
+  {
+    _id: '6222fb24ad4f0b119bedcaf2',
+    created_at: '2022-03-05T05:48:13.190430',
+    updated_at: '2022-03-05T05:48:13.190498',
+    email: 'ledge@gmail.com',
+    name: 'Test Expense 3',
+    description: 'Testing 3',
+    date: '2022-02-10',
+    price: 200.0,
+    currency: 'CAD',
+    exchange_rate: 1.0,
+    location: null,
+    location_x: null,
+    location_y: null,
+    category: 'Food',
+    sub_category: null,
+  },
+  {
+    _id: '6222fb24ad4f0b119bedcaf2',
+    created_at: '2022-03-05T05:48:13.190430',
+    updated_at: '2022-03-05T05:48:13.190498',
+    email: 'ledge@gmail.com',
+    name: 'Test Expense 3',
+    description: 'Testing 3',
+    date: '2022-03-02',
+    price: 435.46,
+    currency: 'CAD',
+    exchange_rate: 1.0,
+    location: null,
+    location_x: null,
+    location_y: null,
+    category: 'Food',
+    sub_category: null,
+  },
+  {
+    _id: '6222fb24ad4f0b119bedcaf2',
+    created_at: '2022-03-05T05:48:13.190430',
+    updated_at: '2022-03-05T05:48:13.190498',
+    email: 'ledge@gmail.com',
+    name: 'Test Expense 3',
+    description: 'Testing 3',
+    date: '2022-03-07',
+    price: 143.46,
+    currency: 'CAD',
+    exchange_rate: 1.0,
+    location: null,
+    location_x: null,
+    location_y: null,
+    category: 'Food',
+    sub_category: null,
+  },
+];
 
 const AnalyticsPage = () => {
   const [compressedData, setCompressedData] = useState([]);
   const [processedData, setProcessedData] = useState([]);
 
   const handleProcessingData = (data) => {
-    let selectedData = data.map((obj) => {
-      return ({
-        x: obj["date"],
-        y: obj["price"]
-      })
-    })
+    const selectedData = data.map((obj) => {
+      return {
+        x: obj.date,
+        y: obj.price,
+      };
+    });
 
-    // Make sure there is only one entry for each date. 
+    // Make sure there is only one entry for each date.
     // If there are more than one entry with the same date, compress them into one entry
     let temp = [];
     selectedData.forEach((row) => {
-      if (temp.length <= 0){
+      if (temp.length <= 0) {
         temp.push(row);
       } else {
-        let previous = temp.pop();
-        if (previous["x"] === row["x"]){
-          previous["y"] = previous["y"] + row["y"];
+        const previous = temp.pop();
+        if (previous.x === row.x) {
+          previous.y += row.y;
           temp.push(previous);
         } else {
           temp.push(previous);
           temp.push(row);
         }
       }
-    })
+    });
     temp = temp.map((row) => {
-      return ({
+      return {
         ...row,
-        y: row["y"].toFixed(2)
-      }
-    )
-    })
+        y: row.y.toFixed(2),
+      };
+    });
     setCompressedData(temp);
 
     // The graph needs data to be passed in as a date, so we convert from datestring into a date object here
     temp = temp.map((obj) => {
-      return({
-        x: new Date(obj["x"]),
-        y: obj["y"]
-      })
-    })
+      return {
+        x: new Date(obj.x),
+        y: obj.y,
+      };
+    });
 
     setProcessedData(temp);
-  }
+  };
 
   useEffect(() => {
     // Make a call to the API here! Then pass it on to the functions below:
     handleProcessingData(unprocessedData);
-  }, [])
+  }, []);
 
   return (
     <View style={styles.container}>
-      <CustomAreaGraph dateStringData={compressedData} dateData = {processedData} />
+      <CustomAreaGraph dateStringData={compressedData} dateData={processedData} />
     </View>
   );
 };
@@ -226,20 +225,20 @@ const styles = StyleSheet.create({
     flexGrow: 1,
   },
   priceHeader: {
-    backgroundColor: "#24838F",
+    backgroundColor: '#24838F',
     fontSize: 36,
     fontWeight: '600',
     color: 'white',
     textAlign: 'center',
-    paddingTop: 60
+    paddingTop: 60,
   },
   dateHeader: {
-    backgroundColor: "#24838F",
+    backgroundColor: '#24838F',
     justifyContent: 'center',
     fontSize: 18,
     fontWeight: '600',
     color: 'white',
-    textAlign: 'center'
+    textAlign: 'center',
   },
 });
 
