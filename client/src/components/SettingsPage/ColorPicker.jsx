c46cd03a037eff9fbacb3a736a855304ece06014
patrzykat <ilyapatrzykat@gmail.com>
2022-03-17 13:50:45 -0700

    added category settings

---
+++ client/src/components/SettingsPage/ColorPicker.jsx
@@ -0,0 +1,168 @@
+import React from 'react';
+import {
+  Dimensions,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  ScrollView,
+} from 'react-native';
+import Modal from 'react-native-modal';
+import theme from '../../../theme';
+import StyledButton from '../StyledButton';
+import axios from '../../providers/axios';
+
+const ColorPicker = ({
+  colorPickerToggle,
+  setColorPickerToggle,
+  expenseToggle,
+  currentCategoryName,
+}) => {
+  const URL = process.env.SERVER_URL;
+
+  const colors = [
+    ['Default', '#A5A7EA'],
+    ['Red', '#EF857D'],
+    ['Orange', '#EEC26C'],
+    ['Blue', '#92CEEF'],
+    ['Yellow', '#E1F17F'],
+    ['Green', '#A2D181'],
+    ['Purple', '#CBA1E6'],
+    ['Pink', '#EDB2C7'],
+    ['Grey', '#919191'],
+  ];
+
+  const changeExpenseCategoryColor = (newCategoryColorHexCode) => {
+    return axios.put(
+      `${URL}/expense_categories/change_color`,
+      {},
+      {
+        params: {
+          category_name: currentCategoryName,
+          category_color: newCategoryColorHexCode,
+        },
+      },
+    );
+  };
+
+  const changeIncomeCategoryColor = (newCategoryColorHexCode) => {
+    return axios.put(
+      `${URL}/income_categories/change_color`,
+      {},
+      {
+        params: {
+          category_name: currentCategoryName,
+          category_color: newCategoryColorHexCode,
+        },
+      },
+    );
+  };
+
+  const changeCategoryColor = async (colorHexCode) => {
+    if (expenseToggle) {
+      await changeExpenseCategoryColor(colorHexCode);
+    } else {
+      await changeIncomeCategoryColor(colorHexCode);
+    }
+    setColorPickerToggle(false);
+  };
+
+  return (
+    <Modal
+      style={{ width: Dimensions.get('window').width, marginLeft: 0 }}
+      isVisible={colorPickerToggle}
+      animationIn="slideInRight"
+      animationOut="slideOutRight"
+      backdropTransitionInTiming={0}
+      backdropColor={theme.colors.white}
+      backdropOpacity={1}>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <StyledButton
+            customStyles={{
+              pressable: {
+                position: 'absolute',
+                left: 5,
+                top: -10,
+              },
+              background: {
+                padding: 20,
+              },
+            }}
+            onTap={() => setColorPickerToggle(false)}
+            iconName="chevron-with-circle-left"
+            iconSize={36}
+            iconColor={theme.colors.primary}
+          />
+          <Text style={styles.titleText}>Categories</Text>
+        </View>
+        <ScrollView style={styles.content}>
+          {colors.map((color) => {
+            return (
+              <Pressable key={color} onPress={() => changeCategoryColor(color[1])}>
+                <View style={styles.setSettingOptions}>
+                  <Text style={styles.optionText}>{color[0]}</Text>
+                  <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                    <View
+                      style={{
+                        backgroundColor: color[1],
+                        borderRadius: 20,
+                        width: 40,
+                        height: 40,
+                        marginRight: 20,
+                      }}
+                    />
+                  </View>
+                </View>
+              </Pressable>
+            );
+          })}
+        </ScrollView>
+      </SafeAreaView>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+    margin: 0,
+  },
+  content: {
+    display: 'flex',
+    marginTop: 10,
+  },
+  header: {
+    display: 'flex',
+    flexDirection: 'row',
+    position: 'relative',
+    justifyContent: 'center',
+  },
+  optionText: {
+    fontSize: 20,
+    color: theme.colors.primary,
+    fontWeight: '600',
+  },
+  setSettingOptions: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 18,
+    paddingHorizontal: 15,
+    borderTopWidth: 1,
+    borderColor: theme.colors.primary,
+  },
+  titleText: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginVertical: 20,
+    color: theme.colors.primary,
+  },
+});
+
+export default ColorPicker;
--- server/src/models/user.py
+++ server/src/models/user.py
@@ -5,18 +5,18 @@ from .category import Category
 from typing import Optional, List
 
 default_expense_categories = [
-    Category(**{"name": "Housing", "color": "blue"}),
-    Category(**{"name": "Transportation", "color": "orange"}),
-    Category(**{"name": "Food", "color": "green"}),
-    Category(**{"name": "Entertainment", "color": "red"}),
-    Category(**{"name": "Other", "color": "grey"}),
+    Category(**{"name": "Housing", "color": "#92CEEF"}),
+    Category(**{"name": "Transportation", "color": "#EEC26CDB"}),
+    Category(**{"name": "Food", "color": "#A2D181"}),
+    Category(**{"name": "Entertainment", "color": "#EF857D"}),
+    Category(**{"name": "Other", "color": "#A5A7EA"}),
 ]
 default_income_categories = [
-    Category(**{"name": "Exployment", "color": "blue"}),
-    Category(**{"name": "Business", "color": "orange"}),
-    Category(**{"name": "Investment", "color": "green"}),
-    Category(**{"name": "Scholarships", "color": "red"}),
-    Category(**{"name": "Other", "color": "grey"}),
+    Category(**{"name": "Exployment", "color": "#92CEEF"}),
+    Category(**{"name": "Business", "color": "#EEC26CDB"}),
+    Category(**{"name": "Investment", "color": "#A2D181"}),
+    Category(**{"name": "Scholarships", "color": "#EF857D"}),
+    Category(**{"name": "Other", "color": "#A5A7EA"}),
 ]
 
 
