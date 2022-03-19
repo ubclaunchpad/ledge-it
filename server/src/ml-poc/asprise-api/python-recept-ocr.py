f31b3b31661727f9e57839941b68ef39d8b55170
mathew bushuru <bushuruwafulamathew@gmail.com>
2022-03-19 09:53:07 -0700

    asprise api

---
+++ server/src/ml-poc/asprise-api/python-recept-ocr.py
@@ -0,0 +1,14 @@
+import requests
+
+print("=== Python Receipt OCR Demo  ===")
+
+receiptOcrEndpoint = 'https://ocr.asprise.com/api/v1/receipt' # Receipt OCR API endpoint
+imageFile = "receipt.jpg" # // Modify this to use your own file if necessary
+r = requests.post(receiptOcrEndpoint, data = { \
+  'client_id': 'TEST',        # Use 'TEST' for testing purpose \
+  'recognizer': 'auto',       # can be 'US', 'CA', 'JP', 'SG' or 'auto' \
+  'ref_no': 'ocr_python_123', # optional caller provided ref code \
+  }, \
+  files = {"file": open(imageFile, "rb")})
+
+print(r.text) # result in JSON
